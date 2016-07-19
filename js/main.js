563ee612d4e2515e760553467faf959126a84e92
Whatever1968 <rodneytrevino@yahoo.com>
2016-07-18 18:20:09 -0700

    JavaScriptPractice

---
+++ js/main.js
@@ -0,0 +1,109 @@
+//var newP = document.createElement("p")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(newP)
+//var textHere = document.createTextNode("Text for paragrpah we created")
+//newP.appendChild(textHere)
+
+
+// Step #1 create the body tag in html
+//<body id="addHere">
+
+// Step #2 Create the element "div", "p", "h1", "whatever"
+//var newDiv = document.createElement("div")
+//
+//// Step #3 Link the id from body to javascript
+//var addHere = document.getElementById("addHere")
+//
+//// Step #4 Link the element "div", "p", "h1", "whatever" to the body tag id 
+//addHere.appendChild(newDiv)
+//
+//// Step #5 Create the text 
+//var textHere = document.createTextNode("Write our text here")
+//
+//// Step #6 Link the text to the "div", "p", "h1", "whatever"
+//newDiv.appendChild(textHere)
+
+
+//var h1 = document.createElement("h1")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(h1)
+//var textHere = document.createTextNode("Paragraph Text for our site.....")
+//h1.appendChild(textHere)
+//<body id="addHere">
+//var newh2 = document.createElement("h2")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(newh2)
+//var textHere = document.createTextNode("am I remembering")
+//newh2.appendChild(textHere)
+
+//var newh3 = document.createElement("h3")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(newh3)
+//var textHere = document.createTextNode("h3 text goes here")
+//newh3.appendChild(textHere)
+//
+//<body id ="addHere">
+//var newDiv =  document.createElement("div")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(newDiv)
+//var textHere = document.createTextNode("try to remember, try to remember")
+//newDiv.appendChild(textHere)
+
+//<body id = "addHere">
+//var newDiv = document.createElement("div")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(newDiv)
+//var textHere = document.createTextNode("this is the text")
+//newDiv.appendChild(textHere)
+
+//var newDiv = document.createElement("div")
+//var addHere = document.getElementById("addHere")
+//addHere.appendChild(newDiv)
+//var textHere = document.createTextNode("create the text here")
+//newDiv.appendChild(textHere)
+
+
+
+// Step #1 create the body tag in html
+//<body id = "addHere">
+
+
+// Step #2 Create the element "div", "p", "h1", "whatever"
+//var newDiv = document.createElement("div")
+//
+//
+//// Step #3 Pull the id from body to javascript
+//var addHere = document.getElementById("addHere")
+//
+//
+//// Step #4 Link the body tag id the element "div", "p", "h1", "whatever" 
+//addHere.appendChild(newDiv)
+//
+//// Step #5 Create the text 
+//var textHere = document.createTextNode("already forgetting what I learned.")
+//
+//
+//// Step #6 Link the element "div", "p", "h1", "whatever" to the Text 
+//newDiv. appendChild(textHere)
+//
+//newDiv.className = "addedClass"
+//
+////<img id = "img"  src = "">
+//var newImg = document.getElementById("img")
+//
+//newImg.setAttribute("src", "img/rod.png")
+//
+//addHere.appendChild(newImgs)
+
+//<body id = "addHere">
+
+var newImg = document.createElement("img")
+newImg.setAttribute("src", "img/rod.png")
+addHere.appendChild(newImg)
+
+
+
+
+
+
+
